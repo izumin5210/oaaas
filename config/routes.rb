9a1189c43c70d7a5dc3d4b13d95955da48e91bc3
izumin5210 <masayuki@izumin.info>
2017-03-09 23:08:33 -0800

    feat(controllers): Add LandingController and DashboardController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
+class AuthenticatedConstraint
+  def matches?(request)
+    request.session['user_id'].present?
+  end
+end
+
 Rails.application.routes.draw do
+  constraints AuthenticatedConstraint.new do
+    root to: 'dashboard#show', as: :user_root
+  end
+
+  root to: 'landing#show'
 
   get '/signin', to: 'sessions#new', as: :signin
   get '/signout', to: 'sessions#destroy', as: :signout
