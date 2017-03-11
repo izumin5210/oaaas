948554bc8f5539cde5493ee21fb59231cb120248
izumin5210 <masayuki@izumin.info>
2017-03-11 00:22:38 -0800

    style(routes): Beautify

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,19 @@ end
 Rails.application.routes.draw do
   root to: 'landing#show'
 
-  get '/signin', to: 'sessions#new', as: :signin
-  get '/signout', to: 'sessions#destroy', as: :signout
+  get '/signin',                  to: 'sessions#new', as: :signin
+  get '/signout',                 to: 'sessions#destroy', as: :signout
   get '/auth/:provider/callback', to: 'sessions#create'
-  get '/auth/failure', to: 'sessions#failure'
+  get '/auth/failure',            to: 'sessions#failure'
 
   constraints AuthenticatedConstraint.new do
-    scope path: '/@:login_name' do
-      root to: 'dashboard#show', as: :user_root
-      get '/:app_name', to: 'applications#show'
-      resources :applications, as: :apps, only: %(show edit update destroy)
-    end
-
-    resources :applications, as: :apps, only: %(new create)
+    root                                   to: 'dashboard#show', as: :user_root
+    post   '/apps',                        to: 'applications#create', as: :create_app
+    get    '/apps/new',                    to: 'applications#new', as: :new_app
+    get    '/@:login_name',                to: 'applications#index', as: :apps
+    delete '/@:login_name/:app_name',      to: 'applications#destroy'
+    patch  '/@:login_name/:app_name',      to: 'applications#update'
+    get    '/@:login_name/:app_name',      to: 'applications#show', as: :app
+    get    '/@:login_name/:app_name/edit', to: 'applications#edit', as: :edit_app
   end
 end
