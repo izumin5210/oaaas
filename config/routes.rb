0fe3f3aa13bc0c6a7d20e34502dca7476a211795
izumin5210 <masayuki@izumin.info>
2017-03-10 23:59:06 -0800

    feat(controllers): Generate ApplicationsController

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,20 @@ class AuthenticatedConstraint
 end
 
 Rails.application.routes.draw do
-  constraints AuthenticatedConstraint.new do
-    root to: 'dashboard#show', as: :user_root
-  end
-
   root to: 'landing#show'
 
   get '/signin', to: 'sessions#new', as: :signin
   get '/signout', to: 'sessions#destroy', as: :signout
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#failure'
+
+  constraints AuthenticatedConstraint.new do
+    scope path: '/@:login_name' do
+      root to: 'dashboard#show', as: :user_root
+      get '/:app_name', to: 'applications#show'
+      resources :applications, as: :apps, only: %(show edit update destroy)
+    end
+
+    resources :applications, as: :apps, only: %(new create)
+  end
 end
